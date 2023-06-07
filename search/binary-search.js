921f7a86492cbfd09a1d8767414b982d6508ece8
Jaeha Lee <wogkdkrm112@gmail.com>
2023-06-08 05:54:37 +0900

    feat: 이진검색 수정

--- search/binary-search.js
+++ search/binary-search.js
@@ -27,26 +27,49 @@
 // binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 28);
 // console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 5));
 
-const binarySearch = (arr, elem) => {
+// const binarySearch = (arr, elem) => {
+//   let start = 0;
+//   let end = arr.length - 1;
+
+//   while (start <= end) {
+//     let middle = Math.floor((start + end) / 2);
+
+//     console.log(start, middle, end);
+
+//     if (arr[middle] === elem) {
+//       return middle;
+//     } else if (elem < arr[middle]) {
+//       end = middle - 1;
+//     } else {
+//       start = middle + 1;
+//     }
+//   }
+
+//   return -1;
+// };
+
+// binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 28);
+// console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 5));
+
+function binarySearch(arr, elem) {
   let start = 0;
   let end = arr.length - 1;
-
-  while (start <= end) {
-    let middle = Math.floor((start + end) / 2);
-
+  let middle = Math.floor((start + end) / 2);
+  while (arr[middle] !== elem && start <= end) {
     console.log(start, middle, end);
-
-    if (arr[middle] === elem) {
-      return middle;
-    } else if (elem < arr[middle]) {
+    if (elem < arr[middle]) {
       end = middle - 1;
     } else {
       start = middle + 1;
     }
+    middle = Math.floor((start + end) / 2);
+  }
+
+  if (arr[middle] === elem) {
+    return middle;
   }
 
   return -1;
-};
+}
 
-binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 28);
-// console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 5));
+console.log(binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 13));
